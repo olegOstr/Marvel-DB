46ab3ed503c7a290879830ce1d5fd6de6c34e50d
Oleg <olego250@gmail.com>
2021-11-04 15:35:23 +0200

    remove public key

--- src/http/MarvelService.js
+++ src/http/MarvelService.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const _API_KEY = 'apikey=8c2b6f490a1cf323e096986a8b2c32dc'
-
 const $host = axios.create({
     baseURL: 'https://gateway.marvel.com:443/v1/public/'
 })
 
 export const fetchAllCharacters = async () => {
-    const {data} = await $host.get(`characters?limit=9&offset=50&${_API_KEY}`)
+    const {data} = await $host.get(`characters?limit=9&offset=50&${process.env.REACT_APP_PUBLIC_API_KEY}`)
     return data.data.results.map(_transformDataChar)
 }
 
 export const fetchCurrentCharacter = async (id) => {
-    const {data} = await $host.get(`characters/${id}?${_API_KEY}`)
+    const {data} = await $host.get(`characters/${id}?${process.env.REACT_APP_PUBLIC_API_KEY}`)
     return _transformDataChar(data.data.results[0])
 }
 
@@ -27,6 +25,6 @@ const _transformDataChar = (char) => {
 }
 
 export const fetchAllComics = async () => {
-    const {data} = await $host.get(`comics?limit=12&${_API_KEY}`)
+    const {data} = await $host.get(`comics?limit=12&${process.env.REACT_APP_PUBLIC_API_KEY}`)
     return data
 }
